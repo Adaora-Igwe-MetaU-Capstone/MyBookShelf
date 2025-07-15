2571f2cb9ddd4957dfbedf950c62cb1f2e21aefc
Adaora Michelle Igwe <adaoraigwe2006@meta.com>
2025-07-15 15:15:58 -0700

    cahngd datasetvand made cahnegs to clean function

--- backend/scripts/cleanBooks.js
+++ backend/scripts/cleanBooks.js
@@ -2,34 +2,45 @@ const fs = require('fs');
 const csv = require('csv-parser');
 
 const results = [];
-const seenTitles = new Set();
+const seenBooks = new Set();
 
-fs.createReadStream('google_books_dataset.csv')
+fs.createReadStream('Books_df.csv')
     .pipe(csv())
     .on('data', (data) => {
-        const rating = parseFloat(data.averageRating);
-        const title = data.title?.trim().toLowerCase();
+        const title = data['Title']?.trim();
+        const author = data['Author']?.trim();
+        const mainGenre = data['Main Genre']?.trim();
+        const subGenre = data['Sub Genre']?.trim();
+        const rating = parseFloat(data.Rating);
+        const numRated = parseInt(data['No. of People rated']);
+        const url = data['URLs']?.trim();
 
         if (
-            title &&
-            data.categories &&
-            !isNaN(rating) &&
-            rating >= 3 &&
-            !seenTitles.has(title)
-        ) {
-            const cleanedCategories = data.categories
-                .split(/[\/,]/)
-                .map(cat => cat.trim().toLowerCase());
+            !title || !author || !mainGenre || !subGenre || isNaN(rating) || isNaN(numRated) || rating < 3 || numRated < 10
+        ) return;
 
-            results.push({
-                title: data.title.trim(),
-                categories: cleanedCategories,
-                averageRating: rating,
-            });
+        const bookKey = `${title.toLowerCase()}|${author.toLowerCase()}`;
+        if (seenBooks.has(bookKey)) return;
 
-            seenTitles.add(title);
-        }
+        seenBooks.add(bookKey);
+
+        const categories = [
+            ...mainGenre.split(',').map(category => category.trim()),
+            ...subGenre.split(',').map(category => category.trim()),
+        ];
+
+        const book = {
+            title,
+            author,
+            categories,
+            averageRating: rating,
+            ratingsCount: numRated,
+            url,
+        };
+
+        results.push(book);
     })
     .on('end', () => {
         fs.writeFileSync('cleaned_books.json', JSON.stringify(results, null, 2));
+
     });
