d86312a1ac5265760ea00dbc06a7c68f9a08fa7b
Adaora Michelle Igwe <adaoraigwe2006@meta.com>
2025-07-15 15:08:21 -0700

    changed daatset and made changes to clean function

--- backend/scripts/cleanBooks.js
+++ backend/scripts/cleanBooks.js
@@ -6,18 +6,31 @@ const results = [];
 fs.createReadStream('google_books_dataset.csv')
     .pipe(csv())
     .on('data', (data) => {
-        const rating = parseFloat(data.averageRating);
-        if (data.title && data.categories && !isNaN(rating) && rating >= 3) {
-            const cleanedCategories = data.categories
-                .split(/[\/,]/)
-                .map(cat => cat.trim().toLowerCase());
-
-            results.push({
-                title: data.title.trim(),
-                categories: cleanedCategories,
-                averageRating: rating,
-            });
+        const title = data['Title']?.trim()
+        const author = data['Author']?.trim()
+        const mainGenre = data['Main Genre']?.trim()
+        const subGenre = data['Sub Genre']?.trim()
+        const rating = parseFloat(data.Rating);
+        const numRated = parseInt(data['No. of People rated'])
+        const url = data['URLs']?.trim()
+        if (
+            !title || !author || !mainGenre || !subGenre || isNan(rating) || isNan(numRated) || rating < 3 || numRated < 10
+        ) return;
+        const categories = [
+            ...mainGenre.split(',').map(category => category.trim()),
+            ...subGenre.split(',').map(category => category.trim()),
+        ]
+        const book = {
+            title,
+            author,
+            categories,
+            averageRating: rating,
+            ratingsCount: numRated,
+            url,
         }
+        results.push(book);
+
+
     })
     .on('end', () => {
         fs.writeFileSync('cleaned_books.json', JSON.stringify(results, null, 2));
