89c3bc55d43df7c61cc5cb8c1ab511add5ea4654
Adaora Michelle Igwe <adaoraigwe2006@meta.com>
2025-07-24 13:10:01 -0700

    changed how user reviews are displayed

--- frontend/my-bookshelf/src/StarRatingDisplay.jsx
+++ frontend/my-bookshelf/src/StarRatingDisplay.jsx
@@ -1,11 +1,13 @@
+import './StarRatingDisplay.css'
 function StarRatingDisplay({ rating }) {
     return (
         <div>
             {[1, 2, 3, 4, 5].map((star) => (
                 <i
+                    id="star"
                     key={star}
                     className={`fa-star fa${star <= rating ? "s" : "r"}`}
-                    style={{ color: star <= rating ? "gold" : "gray", marginRight: 3 }}
+                    style={{ color: star <= rating ? "gold" : "gray", }}
                 ></i>
             ))}
         </div>
